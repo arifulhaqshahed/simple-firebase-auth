14048f9e866f58efa93e2c84f273c5eb37a4f6e1
arifulhaqshahed <arifulhaqueshahed21@gmail.com>
2021-12-02 23:23:19 +0600

    simple firebase authentication with google and github

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBivB3UQx3snz10BjZbLFvyxRZuwDaEa5E",
+    authDomain: "simple-firebase-auth-e1ee8.firebaseapp.com",
+    projectId: "simple-firebase-auth-e1ee8",
+    storageBucket: "simple-firebase-auth-e1ee8.appspot.com",
+    messagingSenderId: "1076758887705",
+    appId: "1:1076758887705:web:9ce0f540b2c1325aea9988"
+  };
+
+export default firebaseConfig;
